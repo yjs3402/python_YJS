30afafa17c0d6eca925515f4b8735f74aaa5ba87
yjs3402 <08michael@naver.com>
2024-06-19 21:51:57 +0900

    Update 가위바위보5판3선승제.py

--- 가위바위보5판3선승제.py
+++ 가위바위보5판3선승제.py
@@ -1,33 +1,36 @@
-def ro_si_pa(p1, p2):
+def input_p1_p2(name):
+    while True:
+        try:
+            p = int(input(f"{name} > "))
+        except ValueError as exception:
+            print("정수를 입력")
+        else:
+            if 1<=p<=3:
+                break
+            print("1, 2, 3 중에 입력")
+    return p
+
+def who_win(p1, p2, s1, s2):
     if p1==p2:
-        print("DRAW")
+        return ("DRAW", s1, s2)
     elif (((p1-p2+3)%3)==1):
-        print("player1 WIN")
-        win["p1"]+=1
+        return ("player1 WIN", s1+1, s2)
     else:
-        print("player2 WIN")
-        win["p2"]+=1
+        return ("player2 WIN", s1, s2+1)
 
-win = {
-    "p1": 0,
-    "p2": 0
-}
-
-verses = 0
-while True:
-    player1 = int(input("player1 >"))
-    player2 = int(input("player2 >"))
-    ro_si_pa(player1, player2)
-    print(win)
-    verses+=1
-    if verses == 5:
-        print("DRAW!!")
-        break
-    elif win["p1"]==3:
-        print("player1 WIN!!")
-        break
-    elif win["p2"]==3:
-        print("player2 WIN!!")
-        break
+def last_who_win(s1, s2):
+    if s1 == 3:
+        return "Finally player1 WIN!!"
+    elif s2 == 3:
+        return "Finally player2 WIN!!"
+    else:
+        return ""
     
-        
+score1, score2 = 0, 0
+while last_who_win(score1, score2) == "":
+    player1 = input_p1_p2("player1")
+    player2 = input_p1_p2("player2")
+    result, score1, score2 = who_win(player1, player2, score1, score2)
+    print(result, f"\n{score1} : {score2}\n")
+
+print(last_who_win(score1, score2))
