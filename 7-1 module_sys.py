1762e8702fb1d2c31cc2c00bedcc0fcb9c434dfb
yjs3402 <08michael@naver.com>
2024-06-20 18:08:23 +0900

    Add files via upload

---
+++ 7-1 module_sys.py
@@ -0,0 +1,15 @@
+
+import sys
+
+
+print(sys.argv)
+print("---")
+
+
+print("getwindowsversion:()",sys.getwindowsversion())
+print("---")
+print("copyright:",sys.copyright)
+print("---")
+print("versioin:", sys.version)
+
+sys.exit()
