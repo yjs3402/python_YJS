440936bd6639cfa01d90dba5523736cd6dd83f0b
yjs3402 <08michael@naver.com>
2024-06-21 19:59:02 +0900

    Update 7-1 module_datetime활용하여날짜출력.py

--- 7-1 module_datetime활용하여날짜출력.py
+++ 7-1 module_datetime활용하여날짜출력.py
@@ -1,7 +1,17 @@
 import datetime
 
 today = datetime.datetime.now()
+cnt = 0
+now_to_newyear = []
+while True:
+    before = today - datetime.timedelta(days=cnt)
+    MonthDay = int(before.strftime("%m%d"))
+    now_to_newyear.append(before)
+    if MonthDay==101:
+        break
+    else:
+        cnt+=1
 
-for back in range():
-    before = today - datetime.timedelta(days=back)
-    print(before.strftime("%m{} %d{}").format("월","일"))
+newyear_to_now = list(reversed(now_to_newyear))
+for mmdd in newyear_to_now:
+    print(mmdd.strftime("%m{} %d{}").format("월","일"))
