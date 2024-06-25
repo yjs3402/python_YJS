32088edc254f371d58423808b1e74fca8f070d11
yjs3402 <08michael@naver.com>
2024-06-25 16:27:43 +0900

    Add files via upload

---
+++ 8-1 object_3_seperate.py
@@ -0,0 +1,34 @@
+
+def create_student(name, korean, math, english, science):
+    return {
+        "name": name,
+        "korean": korean,
+        "math": math,
+        "english": english,
+        "science": science
+    }
+
+def student_get_sum(student):
+    return student["korean"] + student["math"] +\
+        student["english"] + student["science"]
+
+def student_get_average(student):
+    return student_get_sum(student) / 4
+
+def student_to_string(student):
+    return "{}\t{}\t{}".format(
+        student["name"],
+        student_get_sum(student),
+        student_get_average(student))
+students = [
+    create_student("윤인성", 87, 98, 88, 95),
+    create_student("연하진", 92, 98, 96, 98),
+    create_student("구지연", 76, 96, 94, 90),
+    create_student("나선주", 98, 92, 96, 92),
+    create_student("윤아린", 95, 98, 98, 98),
+    create_student("윤명월", 64, 88, 92, 92),
+]
+
+print("이름", "총점", "평균", sep="\t")
+for student in students:
+    print(student_to_string(student))
---
+++ 8-1 object_4_class.py
@@ -0,0 +1,30 @@
+class Student:
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+    def get_sum(self):
+        return self.korean + self.math +\
+            self.english + self.science
+    def get_average(self):
+        return self.get_sum() / 4
+    def to_string(self):
+        return "{}\t{}\t{}".format(\
+        self.name,\
+        self.get_sum(),\
+        self.get_average())
+
+students = [
+    Student("윤인성", 87, 98, 88, 95),
+    Student("연하진", 92, 98, 96, 98),
+    Student("구지연", 76, 96, 94, 90),
+    Student("나선주", 98, 92, 96, 92),
+    Student("윤아린", 95, 98, 98, 98),
+    Student("윤명월", 64, 88, 92, 92),
+]
+
+print("이름", "총점ㅁ", "평균", sep="\t")
+for student in students:
+    print(student.to_string())
