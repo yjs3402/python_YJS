b892d6ce685f883e3c5a5fa4f396b745b1c91383
yjs3402 <08michael@naver.com>
2024-06-26 20:08:06 +0900

    Add files via upload

---
+++ 8-2 class_var.py
@@ -0,0 +1,22 @@
+class Student:
+    count = 0
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+
+        Student.count += 1
+        print("{}번째 학생이 생성되었다.".format(Student.count))
+
+students = [
+    Student("윤인성", 87, 98, 88, 95),
+    Student("연하진", 92, 98, 96, 98),
+    Student("구지연", 76, 96, 94, 90),
+    Student("나선주", 98, 92, 96, 92),
+    Student("윤아린", 95, 98, 98, 98),
+    Student("윤명월", 64, 88, 92, 92),
+]
+print()
+print("현재 생성된 총 학생 수는 {}명입니다.".format(Student.count))
