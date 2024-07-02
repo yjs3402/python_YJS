fb496511c0c8d524685d5a18a53c745f605abd29
yjs3402 <08michael@naver.com>
2024-07-02 19:52:35 +0900

    Update 파이썬응용 클래스로1대1게임.py

--- 파이썬응용 클래스로1대1게임.py
+++ 파이썬응용 클래스로1대1게임.py
@@ -1,33 +1,36 @@
 import time
 import random
-class player:
+
+class Attacking:
     def __init__(self, hp, damage, defense):
         self.__hp = hp
-        self.damage = damage
-        self.defense = defense
-    def attack(self, target, tar):
-        beforehp = target.hp
-        totaldamage = self.damage * self.critical() * self.miss() * target.defense / 100
-        target.hp -= totaldamage
-        print("입힌데미지 {}".format(totaldamage))
-        print("{} hp: {} >> {}".format(tar,beforehp, target.hp))
+        self.__damage = damage
+        self.__defense = 1 - defense / 100
+    def attack(self):
+        return self.damage * self.critical()
+    def attacked(self, myname, damage):  
+        if(self.miss() == 0):
+            print("<<회피<<")
+            return
+        beforehp = self.hp
+        self.hp -= damage * self.defense
+        self.printhp(myname, beforehp, damage * self.defense)
     def critical(self):
         if random.randrange(4) == 0:
-            print("*치명타*")
+            print("**치명타**")
             return 2
         else:
             return 1
     def miss(self):
-        misspercent = random.randrange(3)
-        if misspercent == 0:
-            print("<<회피<<")
-        return (misspercent!=0)
-    @property
-    def hp(self):
-        return self.__hp
-    @hp.setter
-    def hp(self, value):
-        self.__hp = value
+        return random.randrange(5)
+    def printhp(self, myname, beforehp, damage):
+        print("입힌데미지 {}".format(damage))
+        print("{} hp: {} >> {}".format(myname, beforehp, self.__hp))
+
+class Player(Attacking):
+    def __init__(self, hp, damage, defense):
+        super().__init__(hp, damage, defense)
+        
 def input_status(stat):
     print("{} 스탯".format(stat))
     hp = int(input("hp를 입력 > "))
@@ -36,23 +39,24 @@ def input_status(stat):
     return hp,damage,defense
 #체력, 데미지, 방어력 직접입력
 #치명타: 1/4확률로 데미지 두배
-#회피율: 1/3
+#회피율: 1/5확률로 무효
 hp, damage, defense = input_status("P1")
-P1 = player(hp,damage, defense)
+P1 = Player(hp,damage, defense)
 hp,damage, defense = input_status("P2")
-P2 = player(hp,damage, defense)
+P2 = Player(hp,damage, defense)
 while(P1.hp > 0 and P2.hp > 0):
     print()
     print("P1 공격")
-    P1.attack(P2, "P2")
-    time.sleep(5)
-    print("P2 공격")
-    P2.attack(P1, "P1")
-    time.sleep(1)
+    P2.attacked("P2", P1.attack())
     print()
+    time.sleep(0)
+    print("P2 공격")
+    P1.attacked("P1", P2.attack())
+    print()
+    time.sleep(0)
     print("P1 hp: {}  P2 hp: {}".format(P1.hp, P2.hp))
     print("-----------\n")
-    time.sleep(4)
+    time.sleep(0)
     
 if(P1.hp <= 0 and P2.hp <= 0):
     print("DRAW")
