aa32be54c04f21cbbb334b5aa8e58a59e2ca7b7b
yjs3402 <08michael@naver.com>
2024-07-03 21:53:04 +0900

    Update 파이썬응용 클래스로1대1게임.py

--- 파이썬응용 클래스로1대1게임.py
+++ 파이썬응용 클래스로1대1게임.py
@@ -6,9 +6,33 @@ class SpecialAttack:
         pass
 class NormalAttack:
     def __init__(self, hp, damage, defense):
-        pass
+        self.__hp = hp
+        self.__damage = damage
+        self.__defense = 1 - defense / 100
     def attack(self):
-        return self.damage * self.critical()
+        return self.__damage * self.critical()
+    @property
+    def hp(self):
+        return self.__hp
+    @hp.setter
+    def hp(self, value):
+        self.__hp = value
+    @property
+    def damage(self):
+        return self.__damage
+    @damage.setter
+    def damage(self, value):
+        self.__damage = value
+    @property
+    def defense(self):
+        return self.__defense
+    @defense.setter
+    def defense(self, value):
+        self.__defense = value
+
+class Player(NormalAttack):
+    def __init__(self, hp, damage, defense):
+        super().__init__(hp, damage, defense)
     def attacked(self, myname, damage):  
         if(self.miss() == 0):
             print("<<회피<<")
@@ -16,23 +40,19 @@ class NormalAttack:
         beforehp = self.hp
         self.hp -= damage * self.defense
         self.printhp(myname, beforehp, damage * self.defense)
+    def miss(self):
+        return random.randrange(5)
     def critical(self):
         if random.randrange(4) == 0:
             print("**치명타**")
             return 2
         else:
             return 1
-    def miss(self):
-        return random.randrange(5)
     def printhp(self, myname, beforehp, damage):
         print("입힌데미지 {}".format(damage))
         print("{} hp: {} >> {}".format(myname, beforehp, self.hp))
 
-class Player(NormalAttack):
-    def __init__(self, hp, damage, defense):
-        self.hp = hp
-        self.damage = damage
-        self.defense = defense / 100
+
         
 def input_status(stat):
     print("{} 스탯".format(stat))
@@ -52,14 +72,14 @@ while(P1.hp > 0 and P2.hp > 0):
     print("P1 공격")
     P2.attacked("P2", P1.attack())
     print()
-    time.sleep(4)
+    time.sleep(0)
     print("P2 공격")
     P1.attacked("P1", P2.attack())
     print()
-    time.sleep(4)
+    time.sleep(0)
     print("P1 hp: {}  P2 hp: {}".format(P1.hp, P2.hp))
-    print("-----------\n")
-    time.sleep(2)
+    print("-------------------------\n")
+    time.sleep(0)
     
 if(P1.hp <= 0 and P2.hp <= 0):
     print("DRAW")
