ac03eaeeb9e2a1eb73072e5b69bc16b1ab33200e
yjs3402 <08michael@naver.com>
2024-07-23 05:57:49 +0000

    Add counter

--- 응용_naverwebtoon_images_ipad.py
+++ 응용_naverwebtoon_images_ipad.py
@@ -2,6 +2,6 @@ import requests
 import time
 import os
 
-page_folder = f"/python_YJS/naver_webtoon/{title}"
+page_folder = "./python_YJS/naver_webtoon"
 if not os.path.exists(page_folder):
         os.mkdir(page_folder)
