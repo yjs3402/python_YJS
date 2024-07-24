08a6e8174c8f5b3ca595ac4d6704d90222f00900
yjs3402 <08michael@naver.com>
2024-07-24 21:53:39 +0900

    Add files via upload

--- 응용_youtube_story_thumnail.py
+++ 응용_youtube_story_thumnail.py
@@ -1,50 +1,46 @@
-import requests
-import time
-import os
+# 시작하기전 아래 모듈 설치(cmd창 통해서 입력)
+# pip install selenium subprocess pyperclip webdriver-manager
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import subprocess
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pyperclip
+import requests
+
+subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chromeCookie')
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gekco'}
 
-for i in range(204,215):
-    url = f'https://www.youtube.com/@스토리/videos?app=desktop'
-    site = requests.get(url,headers=headers)
-    source_data = site.text
-    title_start = source_data.find('<meta property="og:title" content="') + len('<meta property="og:title" content="')
-    source_data = source_data[title_start:]
-    title_end = source_data.find(' - ')
-    fulltitle_end = source_data.find('">')
-    title = source_data[:title_end]
-    fulltitle = source_data[:fulltitle_end]
-    print(title)
-    print(fulltitle)
-    page_folder = f"./python_YJS/naver_webtoon/{title}"
-    if not os.path.exists(page_folder):
-        os.mkdir(page_folder)
+url = 'https://www.youtube.com/@%EC%8A%A4%ED%86%A0%EB%A6%AC/videos'
 
-    page_folder = f"./python_YJS/naver_webtoon/{title}/{fulltitle}"
-    if not os.path.exists(page_folder):
-        os.mkdir(page_folder)
+option = Options()
+option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
 
-    
-    content_start = source_data.find('#FFFFFF">') + len('#FFFFFF">')
-    source_data = source_data[content_start:]
-    content_end = source_data.find('</div>')
-    source_data = source_data[:content_end]
-    count = source_data.count('<img src="')
-    for j in range(count):
-        image_start = source_data.find('<img src="') + len('<img src="')
-        source_data = source_data[image_start:]
+#driver = webdriver.Chrome(options=option)
+driver.get(url)
+driver.implicitly_wait(3)
 
-        image_end = source_data.find('"')
-        image = source_data[:image_end]
-        print(i, '-', j+1, image)
-        
+actions = driver.find_element(By.CSS_SELECTOR,'body')
+
+for i in range(5):
+    elem = driver.find_elements(By.CLASS_NAME, 'yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded'.replace(' ','.'))
+    for el in range(len(elem)):
+        thumbnail = elem[el].get_attribute('src')
+        title = elem[el].get_attribute('aria-label')
+        print(thumbnail)
         try:
-            file_name=  f'./python_YJS/naver_webtoon/{title}/{fulltitle}/{fulltitle}{j+1}{image[-4:]}'
-            ss = requests.get(image, headers=headers)
+            file_name=  f'./youtube_thumbnail/{title}.jpg'
+            ss = requests.get(thumbnail, headers=headers)
             file = open(file_name, 'wb')
             file.write(ss.content)
             file.close()
-
         except Exception as e:
             print('에러발생 :',e)
-        time.sleep(0.5)
+    actions.send_keys(Keys.PAGE_DOWN)
+    time.sleep(2)
+    
