5e127464a66c127acbec072a0ebae57d10b49621
yjs3402 <08michael@naver.com>
2024-07-29 19:14:03 +0900

    Update 응용_youtube_story_thumbnail.py
    Add from notebook

--- 응용_youtube_story_thumbnail.py
+++ 응용_youtube_story_thumbnail.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pyperclip
 import requests
+import os
 
 subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chromeCookie')
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gekco'}
@@ -29,32 +30,39 @@ actions = driver.find_element(By.CSS_SELECTOR,'body')
 last_height = driver.execute_script("return document.documentElement.scrollHeight")
 
 already_loaded = 0
+titles = []
+links = []
 while True:
     driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
     time.sleep(3)
     new_height = driver.execute_script("return document.documentElement.scrollHeight")
     elem = driver.find_elements(By.CLASS_NAME, 'yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded'.replace(' ','.'))
-    titles = driver.find_elements(By.ID, 'video-title')
-    video_url = driver.find_elements(By.CLASS_NAME,'ytp-title-link yt-uix-sessionlink')
-    for el in range(len(elem)):
+    titles_links = driver.find_elements(By.ID, 'video-title-link')
+    for el in range(already_loaded,len(elem)):
         thumbnail = elem[el].get_attribute('src')
-        title = titles[el].get_attribute('aria-label')
-        title_end = title.find(' 게시자')
-        title = title[:title_end]
-        print(thumbnail[already_loaded:])
-        print(title[already_loaded:])
+        title = titles_links[el].get_attribute('title')
+        link = titles_links[el].get_attribute('href')
         error_text = ['\\','/',':','*','?','"','<','>']
         for k in error_text:
             if title.find(k) != -1:
-                title = title.replace(k, '', len(title))
+                title = title.replace(k, "", len(title))
+        titles.append(title)
+        links.append(title)
+        print(thumbnail)
+        print(title)
+        page_folder = f"./youtube_thumbnail/{title}"
+        if not os.path.exists(page_folder):
+            os.mkdir(page_folder)
+        
         try:
-            file_name=  f'./youtube_thumbnail/{title}.jpg'
+            file_name=  f'./youtube_thumbnail/{title}/{title}.jpg'
             ss = requests.get(thumbnail, headers=headers)
             file = open(file_name, 'wb')
             file.write(ss.content)
             file.close()
         except Exception as e:
             print('에러발생 :',e)
+        time.sleep(1)
     already_loaded = len(elem)
     if new_height == last_height:
         break
