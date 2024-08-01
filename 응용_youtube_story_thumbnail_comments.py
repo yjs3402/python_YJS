3d8e907eff923dd3a9e3b722792252a04c397c26
yjs3402 <08michael@naver.com>
2024-08-01 19:52:57 +0900

    Update 응용_youtube_story_thumbnail_comments.py

--- 응용_youtube_story_thumbnail_comments.py
+++ 응용_youtube_story_thumbnail_comments.py
@@ -16,76 +16,77 @@ import os
 subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chromeCookie')
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gekco'}
 
-url = 'https://www.youtube.com/@%EC%8A%A4%ED%86%A0%EB%A6%AC/videos'
-
 option = Options()
 option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
+
+url = 'https://www.youtube.com'
 
 driver = webdriver.Chrome(options=option)
 driver.get(url)
 driver.implicitly_wait(3)
 
-last_height = driver.execute_script("return document.documentElement.scrollHeight")
+driver.find_elements(By.CLASS_NAME, 'style-scope yt-chip-cloud-chip-renderer'.replace(' ','.'))
 
-already_loaded = 0
-titles = []
-links = []
-while True:
-    actions = driver.find_element(By.CSS_SELECTOR, 'body')
-    actions.send_keys(Keys.END)
-    time.sleep(5)
-    new_height = driver.execute_script("return document.documentElement.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
-
-video = driver.find_elements(By.ID, 'dismissible')
-for video_num in range(len(video)):
-    print(video_num+1, end=' ')
-    elem = video[video_num].find_element(By.CLASS_NAME, 'yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded'.replace(' ','.'))
-    title_link = video[video_num].find_element(By.ID, 'video-title-link')
-    thumbnail = elem.get_attribute('src')
-    title = title_link.get_attribute('title')
-    link = title_link.get_attribute('href')
-    error_text = ['\\','/',':','*','?','"','<','>','.']
-    for k in error_text:
-        if title.find(k) != -1:
-            title = title.replace(k, "", len(title))
-    titles.append(title)
-    links.append(link)
-    print(title)
-    page_folder = f"./youtube_thumbnail/{title}"
-    if not os.path.exists(page_folder):
-            os.mkdir(page_folder)
-    try:
-        file_name=  f'./youtube_thumbnail/{title}/{title}.jpg'
-        ss = requests.get(thumbnail, headers=headers)
-        file = open(file_name, 'wb')
-        file.write(ss.content)
-        file.close()
-    except Exception as e:
-        print('에러발생 :',e)
-    time.sleep(1)
-    
-for li in range(len(links)):
-    driver.get(links[li])
-    driver.implicitly_wait(5)
-    for i in range(2):
+    titles = []
+    links = []
+    last_height = driver.execute_script("return window.scrollY")
+    while True:
         actions = driver.find_element(By.CSS_SELECTOR, 'body')
-        actions.send_keys(Keys.END)
-        time.sleep(10)
-    time.sleep(10)
-    names = driver.find_elements(By.ID, 'author-text')
-    texts = driver.find_elements(By.CLASS_NAME,'yt-core-attributed-string.yt-core-attributed-string--white-space-pre-wrap')[1:]
-    for profile in range(len(names)):
-        name_url = names[profile].get_attribute('href')
-        name = name_url.find('/@')
-        name_url = name_url[name + 2:]
-        text = texts[profile].text
+        actions.send_keys(Keys.PAGE_DOWN)
+        time.sleep(5)
+        new_height = driver.execute_script("return window.scrollY")
+        if new_height == last_height:
+            break
+        print(new_height - last_height)
+        last_height = new_height
 
-        comment_txt = open(f"./youtube_thumbnail/{titles[li]}/{profile}{name_url}.txt", 'w', encoding='UTF-8')
-        comment_txt.write(text)
-        comment_txt.close()
+    video = driver.find_elements(By.ID, 'dismissible')
+    for video_num in range(len(video)):
+        print(video_num+1, end=' ')
+        elem = video[video_num].find_element(By.CLASS_NAME, 'yt-core-image yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded'.replace(' ','.'))
+        title_link = video[video_num].find_element(By.ID, 'video-title-link')
+        thumbnail = elem.get_attribute('src')
+        title = title_link.get_attribute('title')
+        link = title_link.get_attribute('href')
+        error_text = ['\\','/',':','*','?','"','<','>','.']
+        for k in error_text:
+            if title.find(k) != -1:
+                title = title.replace(k, "", len(title))
+        titles.append(title)
+        links.append(link)
+        print(title)
+        page_folder = f"./youtube_thumbnail/{title}"
+        if not os.path.exists(page_folder):
+                os.mkdir(page_folder)
+        try:
+            file_name=  f'./youtube_thumbnail/{title}/{title}.jpg'
+            ss = requests.get(thumbnail, headers=headers)
+            file = open(file_name, 'wb')
+            file.write(ss.content)
+            file.close()
+        except Exception as e:
+            print('에러발생 :',e)
+        time.sleep(1)
         
-    
+    for li in range(len(links)):
+        driver.get(links[li])
+        driver.implicitly_wait(5)
+        for i in range(2):
+            actions = driver.find_element(By.CSS_SELECTOR, 'body')
+            actions.send_keys(Keys.PAGE_DOWN)
+            time.sleep(7)
+        time.sleep(10)
+        names = driver.find_elements(By.ID, 'author-text')
+        texts = driver.find_elements(By.CLASS_NAME,'yt-core-attributed-string.yt-core-attributed-string--white-space-pre-wrap')[1:]
+        for profile in range(len(names)):
+            name_url = names[profile].get_attribute('href')
+            name = name_url.find('/@')
+            name_url = name_url[name + 2:]
+            text = texts[profile].text
+
+            comment_txt = open(f"./youtube_thumbnail/{titles[li]}/{profile}{name_url}.txt", 'w', encoding='UTF-8')
+            comment_txt.write(text)
+            comment_txt.close()
+            
+        
