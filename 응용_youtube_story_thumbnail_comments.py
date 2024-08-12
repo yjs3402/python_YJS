7e0ac2577d639456d249fd8b0eb12af4ca952ecd
yjs3402 <08michael@naver.com>
2024-08-12 21:56:29 +0900

    Update 응용_youtube_story_thumbnail_comments.py

--- 응용_youtube_story_thumbnail_comments.py
+++ 응용_youtube_story_thumbnail_comments.py
@@ -34,11 +34,13 @@ def start_chrome():
 
     option = Options()
     option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-    return option
+    driver = webdriver.Chrome(options=option)
+    return driver
 # 사이트 열기
 def open_site(driver, url):
     driver.get(url)
     driver.implicitly_wait(3)
+    return driver
 # 파일에서 오류가나는 특수문자 제거
 def del_err_txt(name):
     error_text = ['\\','/',':','*','?','"','<','>','.'] # '.' ..?
@@ -46,14 +48,6 @@ def del_err_txt(name):
         if name.find(k) != -1:
             name = name.replace(k, "")
     return name
-# 유튜브 원하는 카테고리 들어가기
-def youtube_choose_category(driver, text):
-    category = driver.find_elements(By.CLASS_NAME, elem_attri['y카테고리'][0])
-    for i in range(len(category)):
-        if category[i].text == text:
-            choosed_category = category[i]
-            break
-    return choosed_category
 # 현재 페이지에서 스크롤 내리기(몇초 기다리기)
 def scroll_down(drvier, time):
     actions = driver.find_element(By.CSS_SELECTOR, 'body')
@@ -75,16 +69,15 @@ def make_folder(address):
     if not os.path.exists(address):
         os.mkdir(address)
 # 블럭리스트에서 원하는 속성 추출해서 리스트만들기
-def get_attribute_forlist(elements, wanna_del_err_txt, attri, time):
+def get_attribute_forlist(elements, wanna_del_err_txt, attri):
     attributes = []
-    for i in range(len(elements)):
-        attribute = elements[i].get_attribute(attri)
+    for elem in elements:
+        attribute = elem.get_attribute(attri)
         if wanna_del_err_txt == 1:
             attribute = del_err_txt(attribute)
         attributes.append(attribute)
-        driver.implicitly_wait(time)
     return attributes
-
+# 이미지 저장하기
 def save_picture(address_name, picture):
     try:
         ss = requests.get(picture, headers=headers)
@@ -93,7 +86,14 @@ def save_picture(address_name, picture):
         file.close()
     except Exception as e:
         print('에러발생 :',e)
-
+# 원하는 element개수가 로딩될만큼 내리기
+def scroll_untill_load(driver, cnt, elem):
+    elems = driver.find_elements(By.ID, elem)
+    while len(chaneols) < cnt:
+        scroll_down(driver, 5)
+        elems = driver.find_elements(By.ID, elem)
+    return driver
+# 유튜브 댓글 저장하기
 def save_comments(address):
     names = driver.find_elements(By.ID, elem_attri['y댓글-이름'][0])
     texts = driver.find_elements(By.CLASS_NAME, elem_attri['y댓글-텍스트'][0])[1:]
@@ -103,21 +103,26 @@ def save_comments(address):
         name_url = name_url[name + 2:]
         text = texts[profile].text
 
-        comment_txt = open(f'{address}/{profile}{name_url}.txt', 'w', encoding='UTF-8')
+        comment_txt = open(f'{address}/{profile}_{name_url}.txt', 'w', encoding='UTF-8')
         comment_txt.write(text)
         comment_txt.close()
-    
+# 유튜브 원하는 카테고리 들어가기
+def youtube_choose_category(driver, text):
+    category = driver.find_elements(By.CLASS_NAME, elem_attri['y카테고리'][0])
+    for i in range(len(category)):
+        if category[i].text == text:
+            choosed_category = category[i]
+            break
+    click_block(choosed_category)
+    return driver
 # 크롬 열기
-option = start_chrome()
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-driver = webdriver.Chrome(options=option)
+driver = start_chrome()
 
 # 유튜브 열기
-open_site(driver, 'https://www.youtube.com')
+drive = open_site(driver, 'https://www.youtube.com')
 
 # '최근에 업로드된 동영상'카테고리에 들어가기
-recently_uploaded = youtube_choose_category(driver, '최근에 업로드된 동영상')
-click_block(recently_uploaded)
+driver = youtube_choose_category(driver, '최근에 업로드된 동영상')
 
 chaneol_count = int(input("탐색할 채널 개수 입력> "))
 video_count = int(input("채널당 탐색할 영상 개수 입력> "))
@@ -125,35 +130,30 @@ video_count = int(input("채널당 탐색할 영상 개수 입력> "))
 chaneol_names = []
 chaneol_sites = []
 # 원하는개수만큼 채널 블럭을 저장
-chaneols = driver.find_elements(By.ID, elem_attri['y프로필-이름-링크'][0])
-while len(chaneols) < chaneol_count:
-    scroll_down(driver, 7)
-    chaneols = driver.find_elements(By.ID, elem_attri['y프로필-이름-링크'][0]) 
+driver = scroll_untill_load(driver, chaneol_count, elem_attri['y프로필-이름-링크'][0])
 # 채널 블럭에서 채널이름, 사이트 추출
-chaneol_names = get_attribute_forlist(chaneols[:chaneol_count], 1, elem_attri['y프로필-이름-링크'][1], 0)
-chaneol_sites = get_attribute_forlist(chaneols[:chaneol_count], 0, elem_attri['y프로필-이름-링크'][2], 0)
+chaneols = driver.find_elements(By.ID, elem)
+chaneol_names = get_attribute_forlist(chaneols[:chaneol_count], 1, elem_attri['y프로필-이름-링크'][1])
+chaneol_sites = get_attribute_forlist(chaneols[:chaneol_count], 0, elem_attri['y프로필-이름-링크'][2])
 for i in range(chaneol_count):
     make_folder(f"./youtube_thumbnail/{chaneol_names[i]}") # 각 채널이름으로 폴더생성
 
 # 각 채널 썸네일,댓글 가져오기
 for i in range(chaneol_count):
     print(i+1, chaneol_names[i])
-    open_site(driver, f'{chaneol_sites[i]}/videos')         # 채널 사이트 접속
+    driver = open_site(driver, f'{chaneol_sites[i]}/videos')         # 채널 사이트 접속
     now_file = f"./youtube_thumbnail/{chaneol_names[i]}"
-    
-    video = driver.find_elements(By.ID, elem_attri['y영상'][0])
-    while len(video) < video_count:
-        scroll_down(driver, 3)
-        video = driver.find_elements(By.ID, elem_attri['y영상'][0])
+
+    driver = scroll_untill_load(driver, video_count, elem_attri['y영상'][0])
     
     elems = driver.find_elements(By.CLASS_NAME, elem_attri['y영상-썸네일'][0])[:video_count]
     title_link = driver.find_elements(By.ID, elem_attri['y영상-제목-링크'][0])[:video_count]
     
-    thumbnails = get_attribute_forlist(elems, 0, elem_attri['y영상-썸네일'][1], 2)
-    titles = get_attribute_forlist(title_link, 1, elem_attri['y영상-제목-링크'][1], 0)
-    links = get_attribute_forlist(title_link, 0, elem_attri['y영상-제목-링크'][2], 0)
+    thumbnails = get_attribute_forlist(elems, 0, elem_attri['y영상-썸네일'][1])
+    titles = get_attribute_forlist(title_link, 1, elem_attri['y영상-제목-링크'][1])
+    links = get_attribute_forlist(title_link, 0, elem_attri['y영상-제목-링크'][2])
     for video_num in range(video_count):
-        open_site(driver, links[video_num])
+        driver = open_site(driver, links[video_num])
         for i in range(2):
             scroll_down(driver, 7)
         time.sleep(10)
